b1369f567451aa2c5f2036d5262efa7ba9332926
Kevin <kevtehpro35@gmail.com>
2015-12-12 10:46:05 -0500

    Added queuing

--- server.go
+++ server.go
@@ -2,10 +2,43 @@ package main
 
 import (
     "github.com/go-martini/martini"
-
+    "github.com/gorilla/websocket"
     "github.com/martini-contrib/render"
 
+    "net"
+    "sync"
     "net/http"
+    "encoding/json"
+)
+
+type Game struct {
+    stage int
+
+    players []ClientConn
+}
+
+type ClientConn struct {
+    name string
+
+    websocket *websocket.Conn
+    clientIP net.Addr
+}
+
+type Packet struct {
+    Action string
+    Payload map[string]interface{}
+}
+
+func (p Packet) toJson() []byte {
+    encode, _ := json.Marshal(p)
+    return encode
+}
+
+var (
+    games map[ClientConn]Game
+    queue []ClientConn
+
+    RWMutex sync.RWMutex
 )
 
 func main() {
@@ -20,8 +53,72 @@ func main() {
 
 func handlers(m *martini.ClassicMartini) {
     m.Get("/", homePage)
+
+    m.Get("/websocket", websocketConn)
 }
 
-func homePage(r *http.Request, w http.ResponseWriter, ren render.Render) {
+func homePage(ren render.Render) {
     ren.HTML(200, "home", nil)
+}
+
+func addToQueue(sockCli ClientConn) {
+    RWMutex.Lock()
+    queue = append(queue, sockCli)
+
+    if len(queue) >= 2 {
+        game := Game {
+            stage: 0,
+            players: []ClientConn {queue[0], queue[1]},
+        }
+        games[queue[0]] = game
+        games[queue[1]] = game
+
+        for i := 0; i != 2; i++ {
+            queue[i].websocket.WriteMessage(1, Packet {
+                Action: "Found Game",
+                Payload: map[string]interface{} {},
+            }.toJson())
+        }
+
+        queue = queue[2:]
+    }
+
+    RWMutex.Unlock()
+}
+
+func removeFromQueue(sockCli ClientConn) {
+    RWMutex.Lock()
+    index := getIndexOf(queue, sockCli)
+    queue = append(queue[:index - 1], queue[index + 1:]...)
+    RWMutex.Unlock()
+}
+
+func websocketConn(r *http.Request, w http.ResponseWriter, ren render.Render) {
+    ws, err := websocket.Upgrade(w, r, nil, 1024, 1024)
+    if _, ok := err.(websocket.HandshakeError); ok || err != nil {
+        return
+    }
+    _, raw, _ := ws.ReadMessage()
+
+    client := ws.RemoteAddr()
+    sockCli := ClientConn{string(raw), ws, client}
+
+    addToQueue(sockCli)
+
+    for {
+        _, _, err := ws.ReadMessage()
+        if err != nil {
+            removeFromQueue(sockCli)
+            return
+        }
+    }
+}
+
+func getIndexOf(conns []ClientConn, conn ClientConn) int {
+    for i, c := range conns {
+        if c.name == conn.name {
+            return i
+        }
+    }
+    return -1
 }
