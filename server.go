a364d747e34e4b33969ffdb400d13d815cbd1bda
Kevin <kevtehpro35@gmail.com>
2015-12-12 09:33:20 -0500

    Basic layout

--- server.go
+++ server.go
@@ -2,12 +2,26 @@ package main
 
 import (
     "github.com/go-martini/martini"
+
+    "github.com/martini-contrib/render"
+
+    "net/http"
 )
 
 func main() {
     m := martini.Classic()
-    m.Get("/", func() string {
-        return "Hello, web!"
-    })
+    m.Use(render.Renderer(render.Options {
+        Directory: "templates",
+        Layout: "layout",
+    }))
+    handlers(m)
     m.Run()
+}
+
+func handlers(m *martini.ClassicMartini) {
+    m.Get("/", homePage)
+}
+
+func homePage(r *http.Request, w http.ResponseWriter, ren render.Render) {
+    ren.HTML(200, "home", nil)
 }
